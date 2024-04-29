e7985bc0a60225705e8adf4dec8d40b2a6624de0
Leonid Darovskikh <skyfire2008@gmail.com>
2024-04-29 21:05:29 +0200

    added some experiment results and a script

---
+++ scripts/roundCsv.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const fs = require("fs").promises;
+
+const path = process.argv[2];
+
+if (path == null) {
+	console.error("no path to csv file");
+	process.exit(1);
+}
+
+fs.readFile(path).then((buf) => {
+	const file = buf.toString();
+	const lines = file.split("\n");
+	for (const line of lines) {
+		const words = line.split(",");
+		for (const word of words) {
+			const num = Number.parseFloat(word);
+			if (!isNaN(num)) {
+				process.stdout.write(`${Math.round(num * 100) / 100}`);
+			} else {
+				process.stdout.write(word);
+			}
+
+			process.stdout.write(",");
+		}
+		process.stdout.write("\n");
+	}
+});
